c611456c0b725d3baa4ad83ca4403aca84d8d325
yasuaki sera <yasuaki.sera15@gmail.com>
2024-02-26 22:39:27 +0900

    update createCloud9.sh: cover non SSMInstanceCore managed role Account

--- 1_create_cloud9/createCloud9.sh
+++ 1_create_cloud9/createCloud9.sh
@@ -3,6 +3,7 @@ STACKNAME="eks-handson-cloud9"
 CLOUD9_OWNER_ARN=$(aws sts get-caller-identity --query 'Arn' --output text)
 TAG_NAME="eks-handson-cloud9"
 
+aws iam create-role --role-name AWSCloud9SSMAccessRole --path /service-role/ --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{"Effect": "Allow","Principal": {"Service": ["ec2.amazonaws.com","cloud9.amazonaws.com"]},"Action": "sts:AssumeRole"}]}'
 aws cloudformation deploy --template-file cloud9_template.yaml \
   --stack-name "${STACKNAME}" \
   --parameter-overrides Cloud9OwnerArn="${CLOUD9_OWNER_ARN}" TagName="${TAG_NAME}"
